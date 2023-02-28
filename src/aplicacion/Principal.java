2887b64115fc92e8e422b9ac9fe32809a4798046
AlejandroLilloRodriguez <alejandro.lillorodriguez@usp.ceu.es>
2023-02-28 10:13:58 +0100

    Ejercicio 13 terminado

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,2 +1,36 @@
-package aplicacion;public class Principal {
+package aplicacion;
+import java.util.Scanner;
+import dominio.Matematicas;
+
+public class Principal {
+
+        public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("introduce un numero entero");
+        int n = sc.nextInt();
+        int[] lista ={1,2,3,4,5,6,7,8,9,10};
+                double media=Matematicas.media(lista);
+                double alcuadrado=Matematicas.media(lista);
+                double varianza=(alcuadrado/lista.length)-media*media;
+                double desviaciontipica=Math.sqrt(varianza);
+        System.out.println("la suma de los numeros enteros desde 0 hasta " + n + " es " + Matematicas.suma(n));
+        System.out.println("la multiplicacion de los numeros enteros desde 1 hasta " + n + " es " + Matematicas.multiplicacion(n));
+        System.out.print("introduce un numero entero");
+        int m = sc.nextInt();
+        System.out.println("la potencia de " + n + " elevado a " + m + " es " + Matematicas.potencia(n, m));
+        System.out.println("la suma de los elementos de la lista es " + Matematicas.sumaLista(lista));
+        System.out.println("la media de los elementos de la lista es " + Matematicas.media(lista));
+        System.out.println("la desviaion tipica de los elementos de la lista es " + desviaciontipica);
+        System.out.println("la suma de los numeros pares desde 0 hasta " + n + " es " + Matematicas.sumaPares(n));
+        System.out.println("la suma de los numeros pares de la lista es " + Matematicas.sumaListaPares(lista));
+        System.out.println("la lista de numeros pares de la lista es " + Matematicas.listaPares(lista));
+        System.out.println("la lista de numeros pares desde 0 hasta " + n + " es " + Matematicas.listaDePares(n));
+        System.out.println("el producto escalar de la lista 1 y la lista 2 es " + Matematicas.productoEscalar(lista, lista));
+        System.out.println("el numero de fibonacci de " + n + " es " + Matematicas.fibonacci(n));
+        System.out.print("introduce un numero entero");
+        int x=sc.nextInt();
+        System.out.println("la relacion aurea entre dos numeros es " + Matematicas.relacionAurea(x));
+
+        }
+
 }
--- src/dominio/Matematicas.java
+++ src/dominio/Matematicas.java
@@ -1,2 +1,73 @@
-package dominio;public class Matematicas {
+package dominio;
+import java.util.stream.Stream;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+public class Matematicas {
+    public static int suma(int n) {
+        return Stream.iterate(0, x -> x + 1).limit(n + 1).reduce(0, (x, y) -> x + y);
+    }
+
+    public static int multiplicacion(int n) {
+        return Stream.iterate(1, x -> x + 1).limit(n).reduce(1, (x, y) -> x * y);
+    }
+
+    public static int potencia(int n, int m) {
+
+        return Stream.iterate(1, x -> x + 1).limit(m).reduce(1, (x, y) -> x * n);
+    }
+
+    public static int sumaLista(int[] lista) {
+        return Stream.iterate(0, i -> i + 1).limit(lista.length).map(i -> lista[i]).reduce(0, (a, b) -> a + b);
+    }
+
+    public static double media(int[] lista) {
+        return Stream.iterate(0, i -> i + 1).limit(lista.length).map(i -> lista[i]).reduce(0, (x, y) -> (x + y)) / lista.length;
+    }
+
+    public static double alcuadrado(int[] lista) {
+        return Stream.iterate(0, x -> x + 1).limit(lista.length).map(n -> lista[n]).reduce(0, (x, y) -> x + y * y);
+    }
+
+    public static int sumaPares(int n) {
+        return Stream.iterate(0, x -> x + 1).limit(n + 1).filter(x -> x % 2 == 0).reduce(0, (x, y) -> x + y);
+    }
+
+    public static int sumaListaPares(int[] lista) {
+        return Stream.iterate(0, x -> x + 1).limit(lista.length).map(x -> lista[x]).filter(x -> x % 2 == 0).reduce(0, (x, y) -> x + y);
+    }
+
+    public static ArrayList listaPares(int[] lista) {
+        return Stream.iterate(0, x -> x + 1).limit(lista.length + 1).filter(x -> x % 2 == 0).collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    public static ArrayList listaDePares(int n) {
+        return Stream.iterate(0, x -> x + 1).limit(n + 1).filter(x -> x % 2 == 0).collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    public static int productoEscalar(int[] lista1, int[] lista2) {
+        return Stream.iterate(0, i -> i + 1).limit(lista1.length).map(i -> lista1[i] * lista2[i]).reduce(0, (a, b) -> a + b);
+    }
+
+    public static int fibonacci(int n) {
+        return Stream.iterate(new int[]{0, 1}, x -> new int[]{x[1], x[0] + x[1]}).limit(n).map(x -> x[0]).reduce(0, (x, y) -> x + y);
+    }
+
+    public static int fibonacci12() {
+        return Stream.iterate(new int[]{0, 1}, x -> new int[]{x[1], x[0] + x[1]}).limit(12).map(x -> x[0]).reduce(0, (x, y) -> x + y);
+    }
+
+    public static int fibonacci13() {
+        return Stream.iterate(new int[]{0, 1}, x -> new int[]{x[1], x[0] + x[1]}).limit(13).map(x -> x[0]).reduce(0, (x, y) -> x + y);
+    }
+
+    public static boolean relacionAurea(int x) {
+        double a = fibonacci13() / fibonacci12();
+        if (a == x) {
+            return true;
+        } else {
+            return false;
+        }
+
+
+    }
 }
